ab6ce710dc158bb65a14f29577f64985c01e10a7
Andrey-rock <andrey-rok@maol.ru>
2025-11-18 13:43:12 +0300

    Библиотека готова

---
+++ src/main/java/org/example/Writable.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.List;
+
+public interface Writable {
+
+    void writeToFile(List<?> data, String fileName);
+
+}
---
+++ src/main/java/org/example/model/Months.java
@@ -0,0 +1,16 @@
+package org.example.model;
+
+public enum Months {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
